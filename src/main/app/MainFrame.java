f35438559a53d0f2b0e8ca78eccd9df4a0ac4acf
balquin <s2010682@usls.edu.ph>
2026-04-03 15:48:25 +0800

    edited

--- src/main/app/MainFrame.java
+++ src/main/app/MainFrame.java
@@ -20,11 +20,11 @@ public class MainFrame extends JFrame {
         cardLayout = new CardLayout();
         mainPanel = new JPanel(cardLayout);
 
-        mainPanel.add(new DashBoardPanel(this), "Dashboard");
-        mainPanel.add(new BDOPanel(this), "BDO");
-        mainPanel.add(new BPIPanel(this), "BPI");
-        mainPanel.add(new MayaPanel(this), "Maya");
-        mainPanel.add(new AboutPanel(this), "About");
+        mainPanel.add(new DashBoardPanel(this), "dashboard");
+        mainPanel.add(new BDOPanel(this), "bdo");
+        mainPanel.add(new BPIPanel(this), "bpi");
+        mainPanel.add(new MayaPanel(this), "maya");
+        mainPanel.add(new AboutPanel(this), "about");
 
         add(mainPanel);
 
