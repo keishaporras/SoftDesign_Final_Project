bc34f5b44204274123864a1fa4dbe7ca577a5556
balquin <s2010682@usls.edu.ph>
2026-04-03 15:45:01 +0800

    another one

--- src/main/app/MainFrame.java
+++ src/main/app/MainFrame.java
@@ -1,15 +1,46 @@
 package main.app;
 
 import main.panels.*;
+import main.service.FirebaseService;
 
 import javax.swing.*;
 import java.awt.*;
 
-import main.service.FirebaseService;
+public class MainFrame extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
 
-public class MainFrame {
+    public MainFrame() {
+
+        setTitle("Bank Setting");
+        setSize(400, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        mainPanel.add(new DashBoardPanel(this), "Dashboard");
+        mainPanel.add(new BDOPanel(this), "BDO");
+        mainPanel.add(new BPIPanel(this), "BPI");
+        mainPanel.add(new MayaPanel(this), "Maya");
+        mainPanel.add(new AboutPanel(this), "About");
+
+        add(mainPanel);
+
+        cardLayout.show(mainPanel, "Dashboard");
+    }
+
+    public void showPanel(String name) {
+        cardLayout.show(mainPanel, name);
+    }
+
     public static void main(String[] args) {
         FirebaseService.initialize();
         FirebaseService.test();
+
+        SwingUtilities.invokeLater(() -> {
+                new MainFrame().setVisible(true);
+        });
     }
 }
