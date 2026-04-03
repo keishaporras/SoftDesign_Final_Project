2f36befbf71986495b5b424be0dfcb31a906a16c
rynez <s2010188@usls.edu.ph>
2026-04-03 15:13:20 +0800

    update

--- src/main/panels/DashBoardPanel.java
+++ src/main/panels/DashBoardPanel.java
@@ -1,4 +1,22 @@
 package main.panels;
 
+import main.app.MainFrame;
+import javax.swing.*;
+import java.awt.*;
+
+
 public class DashBoardPanel {
+    public DashBoardPanel(MainFrame mainFrame){
+        setLayout(new BorderLayout());
+
+        JLabel label = new JLabel("DashBoardPanel", SwingConstants.Center);
+        add(label, BorderLayout.EAST);
+
+        JButton back = new JButton("Back");
+        back.addActionListener(e -> mainFrame.showPanel("dashboard"));
+
+        add(back, BorderLayout.SOUTH);
+
+
+    }
 }
