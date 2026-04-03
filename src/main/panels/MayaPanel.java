2ee4d976012b2648009e37166e010d0939fe4bed
aifosNS <s2010060@usls.edu.ph>
2026-04-03 14:06:04 +0800

    1

--- src/main/panels/MayaPanel.java
+++ src/main/panels/MayaPanel.java
@@ -1,42 +1,22 @@
 package main.panels;
 
+import main.app.MainFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class MayaPanel extends JPanel {
-    private Jlabel savingsLabel;
-    private Jlabel allowanceLabel;
-    private Jlabel addMoreLabel;
-    private Jlabel totalLabel;
 
-    private double savings=0;
-    private double allowance=0;
-    private double addMore=0;
+    public MayaPanel(MainFrame mainFrame) {
 
-    public MayaPanel(){
         setLayout(new BorderLayout());
 
-        JPanel topPanel = new JPanel(new BorderLayout());
-        topPanel.setBackground(Color.decode("#E5E5E5"));
+        JLabel label = new JLabel("BDO Panel", SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
 
-        ImageIcon mayaIcon = new ImageIcon(getClass().getResource("/images/maya.png"));
-        JLabel logoLabel = new JLabel (mayaIcon);
+        JButton back = new JButton("Back");
+        back.addActionListener(e -> mainFrame.showPanel("dashboard"));
 
-        JPanel userPanel = new JPanel();
-        userPanel.setLayout(new BoxLayout(userPanel, BoxLayout.Y_axis));
-        userPanel.setBackground(Color.decode("#E5E5E5"));
-        JLabel nameLabel = new JLabel("Juan Dela Cruz");
-        JLabel accountLabel = new JLabel(" *** *** 789");
-
-        userPanel.add(nameLabel);
-        userPanel.add(accountLabel);
-        topPanel.add(logoLabel, BorderLayout.WEST);
-        topPanel.add(userPanel, Borderlayout.CENTER);
-
-        add(topPanel, Borderlayout.NORTH);
-
-
-
-
-
+        add(back, BorderLayout.SOUTH);
     }
-
-
 }
