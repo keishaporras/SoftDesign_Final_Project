0e849de0bc4720a0a1fbcc059fa3f533f603b332
reign <s2010083@usls.edu.ph>
2026-04-03 14:37:48 +0800

    BDO PANEL after corrections

--- src/main/panels/BDOPanel.java
+++ src/main/panels/BDOPanel.java
@@ -3,11 +3,11 @@ import main.app.MainFrame;
 import javax.swing.*;
 import java.awt.*;
 
-public class BDOPanel extend JPanel{
+public class BDOPanel extends JPanel{
     public BDOPanel(MainFrame mainFrame) {
         setLayout(new BorderLayout());
 
-        JLabel label = new JLabel("BDO Panel", SwingConstraints.CENTER);
+        JLabel label = new JLabel("BDO Panel", SwingConstants.CENTER);
         add(label, BorderLayout.CENTER);
 
         JButton back = new JButton("Back");
