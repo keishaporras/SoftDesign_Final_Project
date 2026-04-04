9774e075e54253f16afddcbcebd4615f3443d2a9
balquin <s2010682@usls.edu.ph>
2026-04-04 15:24:42 +0800

    edited

--- src/main/panels/AddBankPanel.java
+++ src/main/panels/AddBankPanel.java
@@ -1,7 +1,21 @@
 package main.panels;
 
+import main.app.MainFrame;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class AddBankPanel extends JPanel {
 
+    private MainFrame mainFrame;
+
+    public  AddBankPanel(MainFrame mainFrame){
+        this.mainFrame = mainFrame;
+
+        JPanel header = new JPanel();
+        header.setBackground(Color.decode("C4B98B"));
+        header.setPreferredSize(new Dimension(400,80));
+
+
+    }
 }
