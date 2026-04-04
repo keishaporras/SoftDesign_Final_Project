a57d30f5e0f0301d1e0550ecabb68c57e7811970
balquin <s2010682@usls.edu.ph>
2026-04-04 15:18:24 +0800

    edited

--- src/main/app/MainFrame.java
+++ src/main/app/MainFrame.java
@@ -25,6 +25,7 @@ public class MainFrame extends JFrame {
         mainPanel.add(new BPIPanel(this), "bpi");
         mainPanel.add(new MayaPanel(this), "maya");
         mainPanel.add(new AboutPanel(this), "about");
+        mainPanel.add(new AddBank(this), "addbank");
 
         add(mainPanel);
 
