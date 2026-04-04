0a2f1a22a0a88fc538533ed162cad562fb042e48
balquin <s2010682@usls.edu.ph>
2026-04-04 17:59:41 +0800

    edited

--- src/main/panels/AddBankPanel.java
+++ src/main/panels/AddBankPanel.java
@@ -2,6 +2,7 @@ package main.panels;
 
 import main.app.MainFrame;
 import main.model.Bank;
+import main.model.BankStore;
 
 import javax.swing.*;
 import java.awt.*;
@@ -33,6 +34,25 @@ public class AddBankPanel extends JPanel {
 
         JButton confirm = new JButton("Confrim");
 
-        confirm.addActionListener();
+        confirm.addActionListener(e -> {
+            Bank bank = new Bank(
+                    bankName.getText(),
+                    accNum.getText(),
+                    logoPath[0]
+            );
+
+            BankStore.addBank(bank);
+
+            mainFrame.showPanel("dashboard");
+        });
+
+        add(new JLabel("Bank Name"));
+        add(bankName);
+
+        add(new JLabel("Account Number"));
+        add(accNum);
+
+        add(chooseLogo);
+        add(confirm);
     }
 }
